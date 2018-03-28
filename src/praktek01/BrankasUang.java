0ecb8cd79b5c5322e9b553f335c67d71f5e1b289
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-03-28 11:17:32 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/BrankasUang.java
+++ src/praktek01/BrankasUang.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author esteh
  */
 public class BrankasUang {
+    String Merek;
+    String Jenis;
+    String Spesifikasi;
+    double Ukuran;
+    String Harga;
+    
+    void cetakInfo (){
+        System.out.println("Merek : "+Merek);
+        System.out.println("Jenis : "+Jenis);
+        System.out.println("Spesifikasi : "+Spesifikasi);
+        System.out.println("Ukuran : "+Ukuran);
+        System.out.println("Harga : "+Harga);
+        
+    }
     
 }
